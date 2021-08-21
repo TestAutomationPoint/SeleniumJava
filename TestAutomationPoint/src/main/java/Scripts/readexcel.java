4d689747e694173647645927b6668aadce6d7eb4
Ravi Muduthanapalli <ravimuduthana@gmail.com>
2021-08-21 16:17:17 +0530

    changes

--- TestAutomationPoint/src/main/java/Scripts/readexcel.java
+++ TestAutomationPoint/src/main/java/Scripts/readexcel.java
@@ -3,7 +3,6 @@ package Scripts;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -16,43 +15,36 @@ public class readexcel {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		
 		//Create Input steam 
         File file =    new File("C:\\DjangoProjects\\DjangoSeleniumTutorialPoint\\static\\images\\data.xlsx");
         FileInputStream inputStream = new FileInputStream(file);
         XSSFWorkbook wb=new XSSFWorkbook(inputStream);
         
-       
         //create runtime sheet 
         XSSFSheet sheet=wb.getSheet("Data");
         
         //get row
         XSSFRow row1=sheet.getRow(1);
         
-    
-        //create row cell
-        XSSFCell cell=sheet.getRow(1).createCell(4);
-        cell.setCellValue("testwrite");
-       
+        //get row cell
+        XSSFCell cell=sheet.getRow(1).getCell(1);
+        XSSFCell cell1=sheet.getRow(1).getCell(2);
+        XSSFCell cell2=sheet.getRow(1).getCell(3);
         
-       //Create new row in excel sheet
-        XSSFRow row=sheet.createRow(4);
+        //get cell data
+        String username= cell.getStringCellValue();
+        int Mobilenumber=(int) cell1.getNumericCellValue();
+        Date userlogindate=cell2.getDateCellValue();
+        
+        //print
+        System.out.println(username);
+        System.out.println(Mobilenumber);
+        System.out.println(userlogindate);
         
         
-        row.createCell(0).setCellValue("4");
-        row.createCell(1).setCellValue("riana");
-        row.createCell(2).setCellValue("6666454545");
-        row.createCell(3).setCellValue("7-7-2021");
-        
-      //To write into Excel File
-        FileOutputStream outputStream = new FileOutputStream("C:\\\\DjangoProjects\\\\DjangoSeleniumTutorialPoint\\\\static\\\\images\\\\data.xlsx");
-        wb.write(outputStream);
-        
- 
         //close the streams
         wb.close();
         inputStream.close();
-        outputStream.close();
      
 	}
 
